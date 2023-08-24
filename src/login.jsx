90bf3ff787f6d9291bfe9a015b2717d236c6b2cc
uchakip <syedasamra812@gmail.com>
2023-08-25 03:47:59 +0500

    5th

--- src/login.jsx
+++ src/login.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import {React,  useState } from 'react';
 import './login.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const LoginPage = () => {
+  const navigate= useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    
+    let formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    login(formData).then((data)=>{
+
+      if(data.token){
+        localStorage.setItem("token", data.token);
+        navigate("/home");    
+      }
+    });
+
+    
+  };
   return (
     
   <div className="login-section">     
@@ -12,19 +32,21 @@ const LoginPage = () => {
               <img id="customlogo" src="/pinkblink icon.PNG" alt="logo image" />
               <h2>Log In</h2>
               <div className="inputBox">
-                <input type="text" required />
+                <input type="text" value={username}
+                    onChange={(e) => setUsername(e.target.value)} required />
                 <span>Email</span>
                 <i></i>
               </div>
               <div className="inputBox">
-                <input type="password" required />
+                <input type="password"  value={password}
+                    onChange={(e) => setPassword(e.target.value)} required />
                 <span>Password</span>
                 <i></i>
               </div>
               <div className="links">
                 <a href="#">Forgot Password?</a>
               </div>
-              <input type="submit" value="Login" />
+              <input type="submit" value="Login" onClick={handleLogin} />
               <div className="signup">
                 <p>Don't have an account?</p>
                 <Link className="signupcolor" to='/Signup'>
--- src/signup.jsx
+++ src/signup.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './signup.css'; 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Signup() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleRegister = () => {
+    let formData = new FormData();
+    formData.append("username", username);
+    formData.append("email", email);
+    formData.append("password", password);
+
+    register(formData).then((user)=>{
+      if(user){
+        navigate("/login");
+      }
+    });
+
+  };
   return (
     <div className="login-section"> 
     <div className="box2">
@@ -10,17 +28,20 @@ function Signup() {
         <img id="customlogo" src="/pinkblink icon.PNG" alt="logo image" />
         <h2>Sign Up</h2>
         <div className="inputBox2">
-          <input type="text" name="name" required />
+          <input type="text"  value={username}
+                    onChange={(e) => setUsername(e.target.value)} required />
           <span>Full Name</span>
           <i></i>
         </div>
         <div className="inputBox2">
-          <input type="email" name="email" required />
+          <input type="email"  value={email}
+                    onChange={(e) => setEmail(e.target.value)} required />
           <span>Email Address</span>
           <i></i>
         </div>
         <div className="inputBox2">
-          <input type="password" name="password" required />
+          <input type="password"  required  value={password}
+                    onChange={(e) => setPassword(e.target.value)}/>
           <span>Password</span>
           <i></i>
         </div>
@@ -44,7 +65,7 @@ function Signup() {
           </select>
           <span className="inputHighlight"></span>
         </div>
-        <input type="submit" value="Sign Up" />
+        <input type="submit" value="Sign Up"  onClick={handleRegister} />
         <div className="signup">
           <p>Already have an account?</p>
           <Link className="signupcolor" to='/login'>Login</Link>
